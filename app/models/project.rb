da0fcaab1d3712ae8cf133e23f63563fc6cab3e8
Cesar Valderrama <valderramago@gmail.com>
2015-09-29 12:31:04 -0500

    Changed online to succes for online tu successful, added events after transition

--- app/models/project.rb
+++ app/models/project.rb
@@ -309,7 +309,7 @@ class Project < ActiveRecord::Base
         project.expired? && !project.pending_backers_reached_the_goal? && !project.can_go_to_second_chance?
       }
 
-      transition online: :success,      if: ->(project) {
+      transition online: :successful,      if: ->(project) {
         project.expired? && project.reached_goal? && !project.in_time_to_wait?
       }
 
@@ -333,11 +333,11 @@ class Project < ActiveRecord::Base
     after_transition online: :waiting_funds, do: :after_transition_of_online_to_waiting_funds
     after_transition online: :failed, do: :after_transition_of_online_to_failed
     after_transition waiting_funds: [:successful, :failed], do: :after_transition_of_wainting_funds_to_successful_or_failed
-    after_transition waiting_funds: :successful, do: :after_transition_of_wainting_funds_to_successful
+    after_transition [:waiting_funds, :online] => :successful, :do => :after_transition_of_wainting_funds_to_successful
     after_transition draft: :online, do: :after_transition_of_draft_to_online
     after_transition draft: :rejected, do: :after_transition_of_draft_to_rejected
     after_transition any => [:failed, :successful], :do => :after_transition_of_any_to_failed_or_successful
-    after_transition :waiting_funds => [:failed, :successful], :do => :after_transition_of_waiting_funds_to_failed_or_successful
+    after_transition [:online, :waiting_funds] => [:failed, :successful], :do => :after_transition_of_waiting_funds_to_failed_or_successful
     after_transition [:draft, :rejected] => :deleted, :do => :after_transition_of_draft_or_rejected_to_deleted
   end
 
