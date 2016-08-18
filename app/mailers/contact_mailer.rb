51c829f589c1e21ec22af9cf12c27e9f16621501
Cesar Valderrama <valderramago@gmail.com>
2016-08-18 22:48:18 +0000

    Changed email to for backers and contact email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,8 +3,8 @@ class ContactMailer < ActionMailer::Base
   def contact(message_attrs)
     @message = message_attrs
     mail({
-    	from: "#{@message[:name]} <#{@message[:email]}>",
-    	to: "valderramago@gmail.com",
+    	from: "#{::Configuration[:company_name]} <#{::Configuration[:email_system]}>",
+    	to: "#{::Configuration[:email_projects]}",
     	subject: I18n.t('users.contact_message.mail_subject', name: @message[:name], subject: @message[:subject])
     })
   end
