be32f11c17d316fcdeba20770500e24f67557317
Cesar Valderrama <valderramago@gmail.com>
2016-12-28 18:59:59 +0000

    force https for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 # -*- encoding : utf-8 -*-
 class SessionsController < Devise::SessionsController
-  layout 'catarse_bootstrap'
+    layout 'catarse_bootstrap'
+    before_filter :force_https
+    
+    private
+    
+    def force_https
+        if action_name == 'new'
+            redirect_to request.url.sub!('http', 'https') if not request.ssl?
+        end
+    end
 end
