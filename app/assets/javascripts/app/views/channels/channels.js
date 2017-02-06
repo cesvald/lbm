227f9c2ed6dfb1af363c8073d486b52324117524
Cesar Valderrama <valderramago@gmail.com>
2017-02-06 03:16:50 +0000

    Added blue marker for maps

--- app/assets/javascripts/app/views/channels/channels.js
+++ app/assets/javascripts/app/views/channels/channels.js
@@ -29,19 +29,31 @@ CATARSE.channels = {
           slidesPerView: 1,
           slidesPerGroup: 1
         });
-        var handler = Gmaps.build('Google', { markers: { clusterer: { maxZoom: 8, gridSize: 10} } });
-        handler.buildMap({ internal: {id: 'channel-map'}}, function(){
-          var markers = handler.addMarkers([
-            { lat: 4.527468, lng: -75.210700, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
-            { lat: 5.534250, lng: -73.363349, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
-            { lat: 5.565242, lng: -73.337722, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
-            { lat: 5.548498, lng: -73.327422, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
-          ]);
-          handler.map.centerOn({lat: 4.565016, lng: -74.301835});
-          var particularMarker = { lat: 4.530206, lng: -75.177741, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" }
-          handler.addMarker(particularMarker, { fillColor: 'blue' })
+        $('#hidden-marker').load( function(){
+          var handler = Gmaps.build('Google', { markers: { clusterer: { maxZoom: 8, gridSize: 10} } });
+          handler.buildMap({ internal: {id: 'channel-map'}}, function(){
+            var markers = handler.addMarkers([
+              { 
+                lat: 4.527468,
+                lng: -75.210700,
+                name: 'Foo',
+                infowindow: "<p>Este es un proyecto financiado</p><a href='google.com'/>Enlace al proyecto</a>",
+                picture: {
+                  url: $('#hidden-marker').attr('src'),
+                  width:  23,
+                  height: 40
+                }
+              },
+              { lat: 5.534250, lng: -73.363349, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
+              { lat: 5.565242, lng: -73.337722, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
+              { lat: 5.548498, lng: -73.327422, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" },
+            ]);
+            handler.map.centerOn({lat: 4.565016, lng: -74.301835});
+            var particularMarker = { lat: 4.530206, lng: -75.177741, name: 'Foo', infowindow: "<p>Aquí va la información</p><a href='google.com'/>Enlace al proyecto</a>" }
+            handler.addMarker(particularMarker, { fillColor: 'blue' })
+          });
+          handler.getMap().setZoom(6);
         });
-        handler.getMap().setZoom(6);
       },
 
       setupBackground: function(){
